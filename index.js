09f7209772a8c26f291018966d65d319ad251851
David Cervellon <david.cervellon72@gmail.com>
2021-10-21 19:38:14 -0600

    update app

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const http = require('http');
 const PORT = 8080;
 
 http.createServer((request, response) => {
-
+if (request.method === 'GET') {
+    switch (request.url) {
+       case '/':
+           response.end(JSON.stringify({
+               message: 'todolist server'
+           }))
+    }
+}
 }).listen(PORT);
